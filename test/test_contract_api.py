f9a024741a5b2bc5b2e4702f4e0414189a1ee353
Charles Cao <ccao@nbai.io>
2022-07-02 02:06:35 -0400

    add dependency

--- test/test_contract_api.py
+++ test/test_contract_api.py
@@ -5,6 +5,6 @@ from mcs.contract import ContractAPI
 
 @pytest.mark.asyncio
 async def test_approve_usdc():
-    web3_api = "https://polygon-mumbai.g.alchemy.com/***"
+    web3_api = "https://matic-mumbai.chainstacklabs.com"
     api = ContractAPI(web3_api)
     api.approve_usdc("wallet_address", " private_key", "1")
