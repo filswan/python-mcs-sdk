3a89a4427f56edbb97aa432c2cd81dcb50efd09b
daniel <danielw8088@gmail.com>
2022-08-12 10:26:10 +0800

    add install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name="python-mcs-sdk",
-      version="0.0.9",
+      version="0.1.0",
       description="A python software development kit for the Multi-Chain Storage",
       author="daniel8088",
       author_email="danilew8088@gmail.com",
-      requires=["web3", "requests_toolbelt"],
+      install_requires=["web3", "requests", "requests_toolbelt"],
       packages=["mcs", "mcs.api", "mcs.contract", "mcs.contract.abi", "mcs.common"],
       license="MIT",
       include_package_data=True
