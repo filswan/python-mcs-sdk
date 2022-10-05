49919bc9ad7cf996b43789e1a424477a6f879b93
ZihangChen <chenzihang@gmail.com>
2022-10-05 14:55:18 -0400

    removed the mint api test which cannot repeat update mint infos. the update mint info api is still tested in with the contract api mint test

--- test/test_mcs_api.py
+++ test/test_mcs_api.py
@@ -36,11 +36,3 @@ def test_stream_upload_file_pay():
     filepath = "/images/log_mcs.png"
     parent_path = os.path.abspath(os.path.dirname(__file__))
     print(api.stream_upload_file(wallet_address, parent_path + filepath))
-
-
-@pytest.mark.asyncio
-async def test_get_mint_info():
-    api = McsAPI()
-    result = api.get_mint_info(475706, None, "0xd97dcf5d4bdfcc9893a79376c2345994c8599586fbb9ba278815b050c79423df",
-                               "105702", "0x1A1e5AC88C493e0608C84c60b7bb5f04D9cF50B3")
-    print(result)
