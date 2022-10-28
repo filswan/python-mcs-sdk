807b63170491ff1a2eb17b91cefa78d20e4ae194
Zihang Chen <chenzihang@gmail.com>
2022-10-28 13:50:33 -0400

    Add version controll to requirements

--- test/test_mcs_api.py
+++ test/test_mcs_api.py
@@ -7,7 +7,7 @@ from mcs.api import McsAPI
 
 @pytest.mark.asyncio
 async def test_get_params():
-    api_main = McsAPI(Params().MCS_API)
+    api_main = McsAPI(Params('bsc').MCS_API)
     print(api_main.get_params())
 
 
@@ -16,14 +16,14 @@ async def test_user_register():
     load_dotenv(".env_main")
     wallet_address = os.getenv('wallet_address')
     private_key = os.getenv('private_key')
-    api = McsAPI(Params().MCS_API)
-    jwt_token = api.get_jwt_token(wallet_address, private_key, "polygon.mainnet")
+    api = McsAPI(Params('bsc').MCS_API)
+    jwt_token = api.get_jwt_token(wallet_address, private_key, "bsc.testnet")
     print(jwt_token)
 
 
 @pytest.mark.asyncio
 async def test_get_price_rate():
-    api_main = McsAPI(Params('main').MCS_API)
+    api_main = McsAPI(Params('bsc').MCS_API)
     print(api_main.get_price_rate())
 
 @pytest.mark.asyncio
@@ -36,8 +36,8 @@ async def test_upload_file():
     parent_path = os.path.abspath(os.path.dirname(__file__))
 
     # main net test
-    api_main = McsAPI(Params().MCS_API)
-    api_main.get_jwt_token(wallet_address, private_key, "polygon.mainnet")
+    api_main = McsAPI(Params('bsc').MCS_API)
+    api_main.get_jwt_token(wallet_address, private_key, "bsc.testnet")
     print(api_main.upload_file(wallet_address, parent_path + filepath))
 
 
