83076d74b9f6278e4e6115dc29b8a241673a74d1
daniel <danielw8088@gmail.com>
2022-11-11 09:58:08 -0500

    update pypi setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name="python-mcs-sdk",
-      version="0.1.0",
+      version="0.1.5",
       description="A python software development kit for the Multi-Chain Storage",
       author="daniel8088",
       author_email="danilew8088@gmail.com",
       install_requires=["web3", "requests", "requests_toolbelt"],
-      packages=["mcs", "mcs.api", "mcs.contract", "mcs.contract.abi", "mcs.common"],
+      packages=["mcs", "mcs.api", "mcs.contract", "mcs.contract.abi", "mcs.common", "mcs.upload"],
       license="MIT",
       include_package_data=True
       )
