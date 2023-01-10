93017002decf2752050bce66e4dcf04e314ae315
Zihang Chen <chenzihang@gmail.com>
2023-01-10 13:33:02 -0500

    "resolve_conflict"

--- mcs/api/__init__.py
+++ mcs/api/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-from mcs.api.mcs_api import McsAPI
-from mcs.api.bucket_api import BucketAPI
-=======
 from mcs.api.bucket_api import BucketAPI
 from mcs.api.onchain_api import OnchainAPI
->>>>>>> feature/new_structure
