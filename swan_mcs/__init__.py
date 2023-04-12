e9aa29984ef80e48188cfcaed3731175e9b8e182
PlutoNbai <pluto.zhang@nbai.io>
2023-04-12 13:38:15 -0400

    Delete warning

--- swan_mcs/__init__.py
+++ swan_mcs/__init__.py
@@ -1,6 +1,3 @@
 from swan_mcs.api_client import APIClient
 from swan_mcs.api import OnchainAPI
 from swan_mcs.api import BucketAPI
-import warnings
-
-warnings.warn("This version includes breaking changes. Please read the changelog before upgrading.")
